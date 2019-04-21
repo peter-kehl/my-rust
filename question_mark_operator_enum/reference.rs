4b4d1529bcc760317aeac40c192cf45c854cfd56
Peter Kehl <peter.kehl@gmail.com>
2019-04-21 14:21:28 -0700

    Question mark operator

---
+++ question_mark_operator_enum/reference.rs
@@ -0,0 +1,36 @@
+trait Inc {
+    fn inc<'scope>(&self) -> Result<&'scope Self, &'scope str>; //or 'static str instead of 'scope str
+}
+
+#[derive(Debug)]
+enum OneTwoThree {ONE, TWO, THREE, MORE(i32)}
+type OneTwoThreeResult<'scope>= Result<&'scope OneTwoThree, &'static str>;
+
+impl Inc for OneTwoThree {
+    fn inc<'scope>(&self) -> OneTwoThreeResult<'scope> {
+        match self {
+            OneTwoThree::ONE => Ok(&OneTwoThree::TWO),
+            OneTwoThree::TWO => Ok(&OneTwoThree::THREE),
+            OneTwoThree::THREE => Err("Overflown 3"),
+            OneTwoThree::MORE(i) => if i<&std::i32::MAX {
+                Ok( &OneTwoThree::MORE(i+1) )
+            }
+            else {
+                Err("Overflown i32 limit")
+            }
+        }
+    }
+}
+
+fn inc_ok<'scope>() -> OneTwoThreeResult<'scope> {
+    OneTwoThree::ONE.inc()?.inc()
+}
+
+fn inc_overflow<'scope>() -> OneTwoThreeResult<'scope> {
+    OneTwoThree::ONE.inc()?.inc()?.inc()?.inc()?.inc()?.inc()
+}
+
+fn main() {
+    println!( "{:?}", inc_ok());
+    println!( "{:?}", inc_overflow());
+}
