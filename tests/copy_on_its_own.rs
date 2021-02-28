1245e09b1480ba5657cb973b074f9c2087822916
Peter Kehl <peter.kehl@gmail.com>
2021-02-28 11:24:52 -0800

    Copy vs. Move

---
+++ tests/copy_on_its_own.rs
@@ -0,0 +1,23 @@
+use core::ptr::null;
+
+//#[derive(Clone, Copy, Debug)]
+struct SelfPointing {
+    self_ptr: *const SelfPointing
+}
+
+impl SelfPointing {
+    #[inline] // inline doesn't help - the result is still copied
+    pub fn new() -> Self {
+        let mut result = Self {
+            self_ptr: null()
+        };
+        result.self_ptr = &result;
+        result
+    }
+}
+
+#[test]
+fn should_not_copy() {
+    let self_pointed = SelfPointing::new();
+    assert_eq!( &self_pointed as *const SelfPointing, self_pointed.self_ptr);
+}
